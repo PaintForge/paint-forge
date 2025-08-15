7ab602396074564a064909b70cbf487531c501db
PaintForge <paulkocumcmg@gmail.com>
2025-08-15 11:51:48 -0500

    Update home.tsx

--- client/src/pages/home.tsx
+++ client/src/pages/home.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardHeader, CardTitle } from "../components/ui/card";
+import { Button } from "../components/ui/button";
 import { Link } from "wouter";
 import { 
   Package,
