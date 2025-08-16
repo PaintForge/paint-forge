c942d348f26f160d570ac96a8dc1a26c7a6b758c
PaintForge <paulkocumcmg@gmail.com>
2025-08-16 18:50:03 -0500

    Add files via upload

--- client/postcss.config.js
+++ client/postcss.config.js
@@ -1,9 +1,6 @@
-import autoprefixer from '../node_modules/autoprefixer/lib/autoprefixer.js';
-import tailwindcss from '../node_modules/tailwindcss/lib/index.js';
-
 export default {
-  plugins: [
-    tailwindcss,
-    autoprefixer,
-  ],
+  plugins: {
+    tailwindcss: {},
+    autoprefixer: {},
+  },
 };
